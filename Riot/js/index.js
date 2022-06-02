9d0cec6d141842661305751b55a7639dab16067a
giitvijay <gvijay4004@gmail.com>
2022-06-02 22:45:36 +0530

    first commit

---
+++ Riot/js/index.js
@@ -0,0 +1,4 @@
+const getFooterToGoToTop = document.getElementsByClassName('go-up')[0];
+getFooterToGoToTop.addEventListener('click', () => {
+    window.scrollTo('0', '0');
+})
---
+++ Riot/templates/player support/player.js
@@ -0,0 +1,4 @@
+const getCard = document.getElementsByClassName('card')[0];
+const p1 = document.getElementsByClassName('card__player-1')[0];
+getCard.style.height = p1.naturalHeight;
+getCard.style.width = p1.naturalWidth;
